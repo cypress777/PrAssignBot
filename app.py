acf48370f2287be21377607bab554b5cdbbef6aa
ChenYiping <cyp.ress@outlook.com>
2021-08-30 02:36:49 +1000

    [mod] could reply

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import connexion
 
-app = connexion.FlaskApp(__name__, specification_dir="")
-app.add_api("api.yml")
-app.run(port=3978, ssl_context='adhoc')
+
+if __name__ == '__main__':
+    app = connexion.FlaskApp(__name__, specification_dir="")
+    app.add_api("api.yml")
+    app.run(port=3978)
